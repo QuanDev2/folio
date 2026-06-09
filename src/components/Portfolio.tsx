3b2d1185394465196287d3a24161913de10800e9
Quan <qnguyen.dev2@gmail.com>
2026-06-08 20:03:12 -0400

    wire useQuery to Portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,31 +1,54 @@
 import { useParams } from 'react-router-dom'
-import postsData from '../data/posts.json'
 import PostCard from './posts/PostCard'
 import { useCurrentUser } from '../context/CurrentUserContext'
+import { useQuery } from '@tanstack/react-query'
+import type { Post } from '../types'
 
 export default function Portfolio() {
   const { username: paramUsername } = useParams()
   const { user: loggedInUser } = useCurrentUser()
   const isOwner = paramUsername === loggedInUser?.username
 
-  const userPosts = postsData.filter(
-    (post) =>
-      post.authorUsername === paramUsername && (post.published || isOwner)
-  )
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['posts', 'user', paramUsername],
+    queryFn: () =>
+      fetch(`http://localhost:3001/posts?authorUsername=${paramUsername}`).then(
+        (r) => r.json() as Promise<Post[]>
+      ),
+    select: (posts) => {
+      return posts
+        .filter(
+          (post) =>
+            post.authorUsername === paramUsername && (post.published || isOwner)
+        )
+        .map((post) => {
+          const formattedDate = new Intl.DateTimeFormat('en', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric'
+          }).format(new Date(post.createdOn))
+          return { ...post, createdOn: formattedDate }
+        })
+    },
+    staleTime: 60000
+  })
+
+  if (isLoading) return <p>Loading...</p>
+  if (isError || !data) return <p>page not found</p>
 
   return (
     <div>
       <div id='profile-header'>
         <div>username: {paramUsername}</div>
-        <div>post count: {userPosts.length}</div>
+        <div>post count: {data.length}</div>
       </div>
       <div
         id='portfolio-content'
         className='grid gap-5 sm:grid-cols-2 lg:grid-cols-3'
       >
-        {userPosts.length ? (
+        {data.length ? (
           <>
-            {userPosts.map((post) => (
+            {data.map((post) => (
               <PostCard key={post.id} post={post}></PostCard>
             ))}
           </>
